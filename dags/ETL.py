868766c454dddd53510ceb4316987e3d7a882b94
Ariq154404 <44269788+Ariq154404@users.noreply.github.com>
2023-09-22 23:04:05 -0400

    "airlfow-cassandra-pipeline-setup"

---
+++ dags/ETL.py
@@ -0,0 +1,115 @@
+import os
+from dotenv import load_dotenv
+import time
+import logging
+from langchain.chains import create_extraction_chain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.chat_models import ChatOpenAI
+from getpass import getpass
+from jobportal import Jsearch
+from langchain.document_loaders import TextLoader
+from database import CassandraSessions
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task
+
+def val_post_process(val,llm):
+    prompt = PromptTemplate(
+    input_variables=["skill"],
+    template="Summarize  skill: {skill},  in two words seperated by a single space.",
+     )
+    chain = LLMChain(llm=llm, prompt=prompt)
+    lst=[]
+    for v in val:
+        skill=v["keyword skill"].split()  
+        if len(skill)>2:
+            v["keyword skill"]=chain.run({'skill':v["keyword skill"]})
+        v["keyword skill"]=v["keyword skill"].lower()
+      
+        lst.append(v["keyword skill"])
+    return lst
+
+def extract_keyword(jd,llm):
+    schema = {
+    "properties": {
+        "keyword skill": {"type": "string"},
+      
+    },
+    "required": ["keyword skill"],
+   } 
+    chain = create_extraction_chain(schema, llm)
+    val=chain.run(jd)
+    return val
+def get_api_data(job_title):
+    jsearch = Jsearch()
+    jobs= jsearch.fetch_job_description(job_title + "in Canada")
+    return jobs
+
+    # last_day_job=cassandra_manager.retrieve_jobs_last_day()
+    # print('DATA of lst day fetched')
+    # print(last_day_job)
+    #return last_day_job        
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+}
+def load_llm(model_name):
+        load_dotenv()
+    #os.environ.get('openai_key') 
+        llm=ChatOpenAI(model=model_name,temperature = 0,
+                          openai_api_key = ,         
+                          max_tokens=3000                 
+                         ) 
+        return llm
+@dag(
+    dag_id='job_extraction_with_task_flow',
+    default_args=default_args,
+    description='A DAG to extract job details',
+    schedule_interval=None,  # This DAG is manually triggered, not scheduled
+    start_date=datetime(2023, 9, 22),
+    catchup=False,
+)
+def start_etl():
+
+    @task(multiple_outputs=True)
+    def get_jobs_from_api():
+        lst=['Machine learning',"Data Science","Software Engineer","Python Developer","Data Engineer"]
+        nlst=[]
+        for v in lst:
+            nlst+=get_api_data(v)
+        print("ALL JOBS",nlst)
+        return {'jobs':nlst}
+
+    @task()
+    def insert_into_database(results):
+        print("Results",results)
+        cassandra_manager=CassandraSessions(keyspace='job_data')
+        for v in results:
+            cassandra_manager.insert_job_data(v)
+    @task(multiple_outputs=True)
+    def extract_keywords_from_jobs(jobs):
+        llm=load_llm('gpt-3.5-turbo')
+        results=[]
+        for job in jobs:
+            try:
+                jd="Job description: "+ job['job_description']
+                val=extract_keyword(jd,llm)
+                result=val_post_process(val,llm)
+                job["skills"]=result
+                results.append(job)
+            except Exception as e:
+                job["skills"]=None
+            results.append(job)
+        print("Results",results)
+        return {'results':results}
+
+    jobs=get_jobs_from_api()
+    results=extract_keywords_from_jobs(jobs['jobs'])
+    insert_into_database(results=results['results'])
+start_etl() 
---
+++ dags/resume_generation.py
@@ -0,0 +1,103 @@
+
+import os
+from dotenv import load_dotenv
+import time
+import logging
+from langchain.chains import create_extraction_chain
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.callbacks.manager import CallbackManagerForLLMRun
+from langchain.chat_models import ChatOpenAI
+from getpass import getpass
+from jobportal import Jsearch
+from langchain.document_loaders import TextLoader
+from database import CassandraSessions
+from airflow import DAG
+from datetime import datetime
+from airflow.operators.python_operator import PythonOperator
+from airflow.decorators import dag, task
+
+def resume_gen(title,skills,company,link,cnt):
+    prompt = PromptTemplate(
+    input_variables=["title", "skills","data"],
+    template="""Information about Ariq:
+{data}
+You are a resume builder. Build a resume template  for Ariq Rahman in latex based on information about Ariq Rahman and based on the below Job title and keyword skills found in job description. Your Job is to create a resume that is best to tackle the ATS resume system.
+Keep the Following into consideration:
+1) Keep Contact Education, Work Experience, Personal Project, and Certification section as part of the resume.
+2) Select at most 4 relevant Personal Projects under the Project subsection and choose relevant projects .
+3) Paraphrase any sentence to densely pack as many keyword skills found in the job description for better chances to pass  the ATS.
+4) Bold and highlight the relevant keyword skills and tools, techs  found in Job Description
+5) Make the English easy .
+6) Start each sentence with Action Verbs found in the Job Description followed by tools used and end with relevant KPIs and highlight the KPIS
+7) Make Bullet points for each sentence under a subsection.
+Job title:{title}
+Keyword skills:{skills}
+""",
+         )
+    llm=load_llm('gpt-3.5-turbo-16k')
+    chain = LLMChain(llm=llm, prompt=prompt)
+    # loader = TextLoader("test/myself.txt")
+    # data=loader.load()
+    with open('/opt/airflow/dags/myself.txt', 'r') as file:
+        data = file.read()
+    val=chain.run({
+    'title': title,
+    'skills': skills,
+    'data':data
+    })
+    
+    val+="Link to apply job :{lnk}".format(lnk=link)
+    # print(val)
+    full_path="/opt/airflow/dags/resumes/Resume_{name}_{count}.txt".format(name=company,count=cnt)
+
+    print("FULL_PATH")
+    print(full_path)
+    with open(full_path, 'w') as file:
+        file.write(val)
+def load_llm(model_name):
+        load_dotenv()
+    #os.environ.get('openai_key') 
+        llm=ChatOpenAI(model=model_name,temperature = 0,
+                          openai_api_key = ,         
+                          max_tokens=5000                 
+                         ) 
+        return llm
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0,
+}
+@dag(
+    dag_id='resume_generation_dag',
+    default_args=default_args,
+    description='a dag to generate resume from keyword',
+    schedule_interval=None,  # This DAG is manually triggered, not scheduled
+    start_date=datetime(2023, 9, 22),
+    catchup=False,
+)
+def start_etl():
+
+    @task(multiple_outputs=True)
+    def get_jobs_from_database():
+        cassandra_manager = CassandraSessions(keyspace='job_data')
+        jobs = cassandra_manager.retrieve_jobs_last_day()
+        return {'jobs':jobs}
+
+  
+    @task()
+    def generate_resume(result):
+        cnt=1
+        for v in result:
+            for key in v:
+                v[key]=str(v[key])
+            resume_gen(v['job_title'],",".join(v["skills"]),v['employer_name'],v['job_apply_link'],cnt)
+            cnt+=1
+            
+
+    jobs=get_jobs_from_database()
+    generate_resume(jobs['jobs'])
+    
+start_etl() 
